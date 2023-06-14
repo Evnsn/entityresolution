50abe564a88441fe1b1576c61aa34cce9a1d6570
atvi <atvi@norceresearch.no>
2023-06-14 15:55:16 +0200

    Added dataset source

--- entityresolution/datasets/fetch_data.py
+++ entityresolution/datasets/fetch_data.py
@@ -8,6 +8,9 @@ from ._utils import (
     delete_directory
     )
 
+"""
+    Dataset source: https://dbs.uni-leipzig.de/research/projects/object_matching/benchmark_datasets_for_entity_resolution
+"""
 
 DBLP_ACM_DETAILS = {
     "url": "https://dbs.uni-leipzig.de/file/DBLP-ACM.zip",
