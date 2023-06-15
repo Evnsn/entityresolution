df9031c5ababdcc69827ed4e2e6c64a734a0738e
atvi <atvi@norceresearch.no>
2023-06-15 10:34:32 +0200

    Cleaned up repository and upd `.gitignor`

--- entityresolution/datasets/_utils.py
+++ entityresolution/datasets/_utils.py
@@ -7,26 +7,56 @@ import zipfile
 import tarfile
 
 import pandas as pd
+from tqdm.notebook import tqdm
 
 CURRENT_PATH = os.path.abspath(__file__)
 TEMP_PATH = os.path.join(os.path.dirname(CURRENT_PATH), "temp_directory")
 
+# def download_and_extract_zip(url, checksum):
+#     response = requests.get(url)  
+#     response.raise_for_status()
+#     zip_content = io.BytesIO(response.content)
+
+#     hasher = hashlib.sha256() # Calculate the SHA256 checksum of downloaded zip file
+#     hasher.update(response.content)
+#     calculated_checksum = hasher.hexdigest()
+
+#     if calculated_checksum != checksum:
+#         raise ValueError(f"Checksum does not match. Downloaded file may be corrupted.\n Checksum should be: '{checksum}' but was '{calculated_checksum}'.")
+
+#     zip_file = zipfile.ZipFile(zip_content)  # Create a ZipFile object 
+#     zip_file.extractall(path=TEMP_PATH)  # Extract the contents
+#     zip_file.close()  # Close the ZipFile object
+#     extracted_files = zip_file.namelist()  
+
+#     return extracted_files
+
 def download_and_extract_zip(url, checksum):
-    response = requests.get(url)  
+    response = requests.get(url, stream=True)
     response.raise_for_status()
-    zip_content = io.BytesIO(response.content)
-
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 KB
+    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+    zip_content = io.BytesIO()
+    
     hasher = hashlib.sha256() # Calculate the SHA256 checksum of downloaded zip file
-    hasher.update(response.content)
-    calculated_checksum = hasher.hexdigest()
 
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        zip_content.write(data)
+        hasher.update(data)
+    
+    progress_bar.close()
+
+    calculated_checksum = hasher.hexdigest()
     if calculated_checksum != checksum:
         raise ValueError(f"Checksum does not match. Downloaded file may be corrupted.\n Checksum should be: '{checksum}' but was '{calculated_checksum}'.")
-
+    
+    zip_content.seek(0)
     zip_file = zipfile.ZipFile(zip_content)  # Create a ZipFile object 
+    extracted_files = zip_file.namelist()
     zip_file.extractall(path=TEMP_PATH)  # Extract the contents
     zip_file.close()  # Close the ZipFile object
-    extracted_files = zip_file.namelist()  
 
     return extracted_files
 
@@ -50,10 +80,36 @@ def download_and_extract_tar(url, checksum):
 
     return extracted_files
 
+def download_text_file(url, checksum, encoding="utf-8"):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 KB
+    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+    content = b''
+
+    hasher = hashlib.sha256()
+
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        hasher.update(data)
+        content += data
+
+    progress_bar.close()
+
+    calculated_checksum = hasher.hexdigest()
+    if calculated_checksum != checksum:
+        raise ValueError(f"Checksum does not match. Downloaded file may be corrupted.\n Checksum should be: '{checksum}' but was '{calculated_checksum}'.")
+
+    file_data = content.decode('utf-8')
+    dataframe = pd.read_csv(io.StringIO(file_data), delimiter=',')  # Update delimiter if needed
+
+    return dataframe
+
 def load_dataframes_from_csv(file_list, encoding="utf-8", extra_path=""):
     dataframes = []
     for i, file in enumerate(file_list):
-        print(i, file)
+        # print(i, file)
         df = pd.read_csv(TEMP_PATH + "/" + file, encoding=encoding)
         dataframes.append(df)
     return tuple(dataframes)
--- entityresolution/datasets/fetch_data.py
+++ entityresolution/datasets/fetch_data.py
@@ -4,12 +4,21 @@ import pandas as pd
 from ._utils import (
     download_and_extract_zip, 
     download_and_extract_tar, 
+    download_text_file,
     load_dataframes_from_csv, 
-    delete_directory
+    delete_directory,
     )
 
 """
     Dataset source: https://dbs.uni-leipzig.de/research/projects/object_matching/benchmark_datasets_for_entity_resolution
+    - Amazone-GoogleProducts
+    - Abt-Buy
+    - Affiliations
+    - Geographical Settlements
+    - Music Brainz 20K
+    - Music Brainz 200K
+    - Music Brainz 2M
+    - North Carolina Voters 5M
 """
 
 DBLP_ACM_DETAILS = {
@@ -24,6 +33,20 @@ DBLP_SCHOLAR_DETAILS = {
 
 # ...
 
+MUSIC_BRAINZ_20K_DETAILS = {
+    "url": "https://www.informatik.uni-leipzig.de/~saeedi/musicbrainz-20-A01.csv.dapo",
+    "checksum": "527a94f24f7e813a9bc3fef35a635f13e195516966b308140a0dd2926afbb97d"
+}
+
+# ...
+
+MUSIC_BRAINZ_20M_DETAILS = {
+    "url": "https://www.informatik.uni-leipzig.de/~saeedi/musicbrainz-20000-A01.csv.dapo",
+    "checksum": "6da9d92d65b55f65cc7b3cdd7acfe38765f25dec388526ddab83b4b8ccfb6ad8"
+}
+
+# ...
+
 NORTH_CAROLINA_VOTERS_10M_DETAILS = {
     "url": "https://www.informatik.uni-leipzig.de/~saeedi/10Party-ocp20.tar.gz",
     "checksum": "8ee9c723e9b74470e828a66608669bdc6213b29df603488eb203e7b8202fe007"
@@ -31,21 +54,35 @@ NORTH_CAROLINA_VOTERS_10M_DETAILS = {
 
 def fetch_DBLP_ACM():
     file_list = download_and_extract_zip(url=DBLP_ACM_DETAILS["url"], checksum=DBLP_ACM_DETAILS["checksum"])
-    dblp_acm = load_dataframes_from_csv(file_list, "ISO-8859-1")
+    df = load_dataframes_from_csv(file_list, encoding="ISO-8859-1")
     delete_directory() # Delet TAMP_PATH
-    return dblp_acm
+    return df
 
 def fetch_DBLP_SCHOLAR():
     file_list = download_and_extract_zip(url=DBLP_SCHOLAR_DETAILS["url"], checksum=DBLP_SCHOLAR_DETAILS["checksum"])
-    dblp_scholar = load_dataframes_from_csv(file_list, "ISO-8859-1")
+    df = load_dataframes_from_csv(file_list, encoding="ISO-8859-1")
     delete_directory() # Delet TAMP_PATH
-    return dblp_scholar
+    return df
+
+def fetch_MUSIC_BRAINZ_20K():
+    # file_list = download_and_extract_zip(url=MUSIC_BRAINZ_20M_DETAILS["url"], checksum=MUSIC_BRAINZ_20M_DETAILS["checksum"])
+    # df = load_dataframes_from_csv(file_list, encoding="ISO-8859-1")
+    df = download_text_file(url=MUSIC_BRAINZ_20K_DETAILS["url"], checksum=MUSIC_BRAINZ_20K_DETAILS["checksum"])
+    # delete_directory() # Delet TAMP_PATH
+    return df
+
+def fetch_MUSIC_BRAINZ_20M():
+    # file_list = download_and_extract_zip(url=MUSIC_BRAINZ_20M_DETAILS["url"], checksum=MUSIC_BRAINZ_20M_DETAILS["checksum"])
+    # df = load_dataframes_from_csv(file_list, encoding="ISO-8859-1")
+    df = download_text_file(url=MUSIC_BRAINZ_20M_DETAILS["url"], checksum=MUSIC_BRAINZ_20M_DETAILS["checksum"])
+    # delete_directory() # Delet TAMP_PATH
+    return df
 
 def fetch_NORTH_CAROLINA_VOTERS_10M():
     file_list = download_and_extract_tar(url=NORTH_CAROLINA_VOTERS_10M_DETAILS["url"], checksum=NORTH_CAROLINA_VOTERS_10M_DETAILS["checksum"])
 
     # Remove elements that are not paths to a '.csv' file
     file_list = [file_name for file_name in file_list if file_name.endswith(".csv")]
-    dblp_scholar = load_dataframes_from_csv(file_list)
+    df = load_dataframes_from_csv(file_list)
     delete_directory() # Delet TAMP_PATH
-    return dblp_scholar
+    return df
