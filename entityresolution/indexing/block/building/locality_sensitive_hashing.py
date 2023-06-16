38136f48a39192921a2712bebdefb19dc28c0cb4
atvi <atvi@norceresearch.no>
2023-06-16 15:46:54 +0200

    Began LSH

---
+++ entityresolution/indexing/block/building/locality_sensitive_hashing.py
@@ -0,0 +1,12 @@
+from ._base import BaseBlocking
+
+"""
+
+Resources:
+- https://www.pinecone.io/learn/locality-sensitive-hashing/
+
+"""
+
+
+class LocalitySensitiveHashing(BaseBlocking):
+    pass
